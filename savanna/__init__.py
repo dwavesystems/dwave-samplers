90d9b2b57ea32ac68edcac317408e7516ced2e90
Alexander Condello <acondello@dwavesys.com>
2018-05-18 13:46:31 -0700

    Add transformation from bqm to agreement weighted graph

--- savanna/__init__.py
+++ savanna/__init__.py
@@ -1,3 +1,6 @@
+from savanna.agreement_ising import *
+import savanna.agreement_ising
+
 from savanna.planar_graphs import *
 import savanna.planar_graphs
 
---
+++ savanna/agreement_ising.py
@@ -0,0 +1,25 @@
+import networkx as nx
+
+
+def bqm_to_agreement_graph(bqm):
+    """todo"""
+    if any(bqm.linear.values()):
+        raise NotImplementedError("not yet implemented for non-zero linear biases")
+
+    offset = bqm.spin.offset
+    G = nx.Graph()
+    for (u, v), bias in bqm.spin.quadratic.items():
+        G.add_edge(u, v, weight=-2 * bias)
+
+        offset += bias
+
+    return G, offset
+
+
+def agreement_energy(state, G, offset=0.0):
+    """todo"""
+    en = offset
+    for u, v, data in G.edges(data=True):
+        if state[u] != state[v]:
+            en += data['weight']
+    return en
