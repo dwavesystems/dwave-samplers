3c9c506a7cabbc652b3b597fd440bed358eebc39
Radomir Stevanovic <radomir@dwavesys.com>
2019-07-18 14:57:38 -0700

    Refactor setup/build_ext for robustness
    - cythonize whenever cython installed
    - OR when explicitly requested (with USE_CYTHON env var), in which case
      setup/distutils installs cython during build
    - if cythonization fails, fallback to pre-cythonized cpp file (if they
      are available)
    - grouped cython-build stuff under build_ext subclass (numpy headers,
      compiler/linker flags, conditional cythonization)

--- setup.py
+++ setup.py
@@ -4,6 +4,59 @@ from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 
 
+class cythonizing_build_ext(build_ext):
+    """Cython extensions adaptive build: cythonize if possible, include numpy
+    headers, add custom compile/link flags."""
+
+    # Custom extension compile/link flags
+    extra_compile_args = {
+        'msvc': ['/std:c++14'],
+        'unix': ['-std=c++11'],
+    }
+
+    extra_link_args = {
+        'msvc': [],
+        'unix': [],
+    }
+
+    def build_extensions(self):
+        # try using cython if available, fallback to using pre-cythonized
+        # files (if they exist; if not, ultimately fail)
+        try:
+            from Cython.Build import cythonize
+
+            # cythonization step will remove this flag needed by setuptools
+            _needs_stub = [ext._needs_stub for ext in self.extensions]
+
+            self.extensions = cythonize(self.extensions)
+
+            # restore _needs_stub flags for setuptools
+            for ext, ns in zip(self.extensions, _needs_stub):
+                ext._needs_stub = ns
+
+        except ImportError:
+            # cython not available, assume cythonized cpp files exist
+            for ext in self.extensions:
+                ext.sources = [source.replace(".pyx", ".cpp") for source in ext.sources]
+
+        # add numpy include files
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+        # add compiler/linker flags
+        compiler = self.compiler.compiler_type
+
+        compile_args = self.extra_compile_args[compiler]
+        for ext in self.extensions:
+            ext.extra_compile_args = compile_args
+
+        link_args = self.extra_compile_args[compiler]
+        for ext in self.extensions:
+            ext.extra_compile_args = link_args
+
+        build_ext.build_extensions(self)
+
+
 # Load package info, without importing the package
 basedir = os.path.dirname(os.path.abspath(__file__))
 package_info_path = os.path.join(basedir, "greedy", "package_info.py")
@@ -14,73 +67,30 @@ try:
 except SyntaxError:
     execfile(package_info_path, package_info)
 
-
 packages = ['greedy']
 
 # Package requirements, minimal pinning
 install_requires = ['six>=1.10', 'numpy>=1.15.0,<1.16.0', 'dimod>=0.8.11,<0.9.0']
-
-# Setup (extension build) requirements
-setup_requires = ['numpy>=1.14.0,<1.16.0']
 
 # Package extras requirements
 extras_require = {
     'test': ['coverage', 'mock'],
 }
 
-
-# Custom build_ext subclass to add compile/link args and numpy include dirs
-extra_compile_args = {
-    'msvc': ['/std:c++14'],
-    'unix': ['-std=c++11'],
-}
-
-extra_link_args = {
-    'msvc': [],
-    'unix': [],
-}
-
-class build_ext_compiler_check(build_ext):
-    def build_extensions(self):
-        compiler = self.compiler.compiler_type
-
-        compile_args = extra_compile_args[compiler]
-        for ext in self.extensions:
-            ext.extra_compile_args = compile_args
-
-        link_args = extra_compile_args[compiler]
-        for ext in self.extensions:
-            ext.extra_compile_args = link_args
-
-        build_ext.build_extensions(self)
-
-    def run(self):
-        import numpy
-        self.include_dirs.append(numpy.get_include())
-        build_ext.run(self)
-
+# Setup (extension build) requirements
+setup_requires = ['numpy>=1.14.0,<1.16.0']
 
 # We distribute cythonized source, so cython is not required
-# for install (build) from package
-if not os.path.exists(os.path.join(basedir, 'PKG-INFO')):
-    try:
-        from Cython.Build import cythonize
-        USE_CYTHON = True
-    except ImportError:
-        USE_CYTHON = False
-else:
-    USE_CYTHON = False
+# for install (build) from package, but allow manual override via env var
+USE_CYTHON = os.getenv('USE_CYTHON', False)
 
-ext = '.pyx' if USE_CYTHON else '.cpp'
+if USE_CYTHON:
+    setup_requires.append('cython>=0.29.12')
 
 extensions = [Extension(
     name='greedy.descent',
-    sources=['./greedy/descent' + ext],
+    sources=['./greedy/descent.pyx'],
 )]
-
-if USE_CYTHON:
-    extensions = cythonize(extensions)
-
 
 classifiers = [
     'License :: OSI Approved :: Apache Software License',
@@ -108,7 +118,7 @@ setup(
     setup_requires=setup_requires,
     extras_require=extras_require,
     ext_modules=extensions,
-    cmdclass={'build_ext': build_ext_compiler_check},
+    cmdclass={'build_ext': cythonizing_build_ext},
     classifiers=classifiers,
     zip_safe=False,
 )
