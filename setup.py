78917f6a29ed3678ef368cf044b7b216a76e3a2e
Alexander Condello <arcondello@gmail.com>
2020-05-13 13:51:46 -0700

    Drop python 2.7 and 3.4 support

--- setup.py
+++ setup.py
@@ -1,32 +1,34 @@
-from __future__ import absolute_import
-
-import sys
-import os
+# Copyright 2020 D-Wave Systems Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
 
 from setuptools import setup
 from setuptools.command.build_ext import build_ext
 from distutils.extension import Extension
-from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
 
 classifiers = [
     'License :: OSI Approved :: Apache Software License',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7'
+    'Programming Language :: Python :: 3.8'
 ]
 
-python_requires = '>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*'
-
-_PY2 = sys.version_info.major == 2
+python_requires = '>=3.5'
 
 # add __version__, __author__, __authoremail__, __description__ to this namespace
-if _PY2:
-    execfile("./orang/package_info.py")
-else:
-    exec(open("./orang/package_info.py").read())
+exec(open("./orang/package_info.py").read())
 
 install_requires = ['numpy>=1.15.0,<2.0.0',
                     'dimod>=0.8.0,<0.9.0',
