15ea4255615cca1e019979b4f03e97a21c0e6138
Heidi Tong <htong@dwavesys.com>
2021-12-20 00:54:20 -0800

    Make use of cppbqm and remove unneeded functions

--- orang/solve.pyx
+++ orang/solve.pyx
@@ -1,116 +1,76 @@
 # distutils: language = c++
 # cython: language_level = 3
-#
-# =============================================================================
+
 # Copyright 2019 D-Wave Systems Inc.
 #
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#        http://www.apache.org/licenses/LICENSE-2.0
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
-#
-# =============================================================================
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import dimod
 import numpy as np
+
 cimport numpy as np
+from dimod cimport cyBQM_float64
+from dimod.libcpp cimport cppBinaryQuadraticModel
+from dimod.binary.binary_quadratic_model import BinaryQuadraticModel
 
-from orang.conversions cimport coo_tables
-from orang.orang cimport tables_type, energies_type, samples_type, solveTables
+from orang.orang cimport energies_type, samples_type, PyArray_ENABLEFLAGS
 
-cdef extern from "numpy/arrayobject.h":
-    void PyArray_ENABLEFLAGS(np.ndarray arr, int flags)
+cdef extern from "src/include/solve.hpp":
+    void solveBQM[V, B](cppBinaryQuadraticModel[B, V]& refBQM,
+                        int* var_order,
+                        double beta,
+                        int low,
+                        double max_complexity,
+                        int max_solutions,
+                        double** energies_data, int* energies_len,
+                        int** sols_data, int* sols_rows, int* sols_cols)
 
 samples_dtype = np.intc  # needs to be consistent with samples_type
 energies_dtype = np.double  # needs to be consistent with energies_type 
 
-def solve_coo(linear, quadratic, offset, vartype, max_complexity, order=None,
-              max_solutions=1):
-    """Find ground states of a binary quadratic model in coo format.
+def solve_bqm_wrapper(bqm: BinaryQuadraticModel, 
+                      order: list,
+                      max_complexity: int,
+                      max_solutions: int = 1):
+    """Cython wrapper for solve_bqm()."""
 
-    Args:
-        linear (array_like):
-            A 1D array-like iterable of linear biases.
+    if not bqm.num_variables:
+        raise ValueError("bqm must have at least one variable.")
 
-        quadratic (tuple[array_like, array_like, array_like]):
-            A 3-tuple of 1D array_like vectors of the form (row, col, bias).
-
-        offset (numeric, optional):
-            Constant offset for the binary quadratic model.
-
-        vartype (:class:`.Vartype`):
-            Variable type for the binary quadratic model. Accepted input values:
-            * :class:`.Vartype.SPIN`
-            * :class:`.Vartype.BINARY`
-
-        max_complexity (int): Upper bound on the exponent of space requirements
-            at each step of the algorithm. Should be larger than the treewidth.
-
-        order (array-like, optional):
-            The elimination order of the variables. If not provided the
-            variables are eliminated in range-order.
-
-        max_solutions (int, optional, default=1):
-            The maximum number of solutions to return.
-
-    Returns:
-        tuple: A 2-tuple containing the samples and energies as numpy arrays.
-
-
-    """
-
-    cdef int num_variables = len(linear)
-    cdef double low = -1 if vartype is dimod.SPIN else 0
+    cdef cyBQM_float64 cybqm = bqm.data
     cdef double beta = -1  # solving
+    cdef double _max_complexity = max_complexity
+    cdef int low = -1 if bqm.vartype is dimod.SPIN else 0
 
-    if not num_variables:
-        # nothing to sample from
-        raise ValueError("coo must contain at least one variable")
+    cdef int[:] elimination_order = np.asarray(order, dtype=np.intc)
+    cdef int* elimination_order_ptr = &elimination_order[0]
 
-    cdef tables_type tables = coo_tables(linear, quadratic, low, beta)
-
-    cdef int[:] elimination_order
-    if order is None:
-        elimination_order = np.arange(num_variables, dtype=np.intc)
-    else:
-        elimination_order = np.asarray(order, dtype=np.intc)
-
-    samples, energies = solve_tables(tables, num_variables, low,
-                                     elimination_order, max_complexity,
-                                     max_solutions)
-
-    energies += offset
-
-    return samples, energies
-
-
-cdef solve_tables(tables_type tables, int num_variables, double low,
-               int[:] elimination_order, double max_complexity,
-               int max_solutions):
-    """Find ground states from the given tables."""
-
-    cdef int elimination_order_length = elimination_order.shape[0]
-    cdef int* elimination_order_pointer = &elimination_order[0]
-
-    # Pass in a pointer so that solveTables can fill it in. Note that this is
+    # Pass in a pointer so that solveBQM can fill it in. Note that this is
     # a design choice inherited from orang's c++ implementation. In the future
     # we may want to change it.
     cdef int num_energies, srows, scols
     cdef energies_type* energies_pointer
     cdef samples_type* samples_pointer
 
-    solveTables(tables, num_variables,
-                 elimination_order_pointer, elimination_order_length,
-                 max_complexity, max_solutions,
-                 &energies_pointer, &num_energies,
-                 &samples_pointer, &srows, &scols
-                 )
+    solveBQM(cybqm.cppbqm,
+             elimination_order_ptr,
+             beta,
+             low,
+             _max_complexity,
+             max_solutions,
+             &energies_pointer, &num_energies,
+             &samples_pointer, &srows, &scols
+            )
 
     # create a numpy array without making a copy then tell numpy it needs to
     # free the memory
@@ -126,6 +86,9 @@ cdef solve_tables(tables_type tables, int num_variables, double low,
                     samples[i, j] = -1
 
     energies = np.asarray(<energies_type[:num_energies]> energies_pointer)
+
     PyArray_ENABLEFLAGS(energies, np.NPY_OWNDATA)
+
+    energies += bqm.offset
 
     return samples, energies
