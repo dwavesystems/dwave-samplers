290b1f505a4aa6163583fc1921c301dbb2602981
John McFarland <jmcfarland@dwavesys.com>
2022-05-03 12:46:25 -0700

    Replace <boost/cstdint.hpp> with <cstdint>

--- orang/src/include/base.h
+++ orang/src/include/base.h
@@ -21,16 +21,15 @@
 #include <cstddef>
 #include <set>
 #include <vector>
-
-#include <boost/cstdint.hpp>
+#include <cstdint>
 
 namespace orang {
 
-typedef boost::uint32_t Var;
+typedef std::uint_least32_t Var;
 typedef std::vector<Var> VarVector;
 typedef std::set<Var> VarSet;
 
-typedef boost::uint16_t DomIndex;
+typedef std::uint_least16_t DomIndex;
 typedef std::vector<DomIndex> DomIndexVector;
 
 typedef std::vector<std::size_t> SizeVector;
