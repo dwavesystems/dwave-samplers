7f94b3c6a1fe36032a8724370e9efa25f9b89f6d
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2022-06-17 06:41:37 -0700

    Swap import between setuptools and cython

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from Cython.Build import cythonize
 from setuptools import setup
+from Cython.Build import cythonize
 from setuptools.command.build_ext import build_ext
 
 import dimod
